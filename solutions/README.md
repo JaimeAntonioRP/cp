aa452bc6b592508dec78d42049e61f1961d9e812
JaimeAntonioRP <jrodriguezphillco@gmail.com>
2026-08-08 17:56:24 -0500

    solve(codeforces): 4A - Watermelon

--- solutions/README.md
+++ solutions/README.md
@@ -4,5 +4,5 @@ Total: **1** problemas.  Generado por `cpx accept` - no editar a mano.
 
 | Fecha | Juez | ID | Problema | Tags | Codigo |
 | --- | --- | --- | --- | --- | --- |
-| 2026-08-08 | leetcode | `1` | Two Sum | hash, array | [codigo](leetcode/1) |
+| 2026-08-08 | codeforces | `4A` | Watermelon | math | [codigo](codeforces/4A) |
 
---
+++ solutions/codeforces/4A/solution.cpp
@@ -0,0 +1,53 @@
+/**
+ *  Autor : JaimeAntonioRP
+ *  Problema:
+ *  Enlace  : https://codeforces.com/problemset/problem/4/A
+ *  Idea    :
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+template <class T> using vc  = vector<T>;
+template <class T> using vvc = vector<vector<T>>;
+template <class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   ((int)(x).size())
+#define pb      push_back
+#define eb      emplace_back
+#define fi      first
+#define se      second
+
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define dbg(...) ((void)0)
+#endif
+
+constexpr int INF = 1'000'000'007;
+constexpr ll  LNF = 4'000'000'000'000'000'000LL;
+
+void solve() {
+    int n; cin >> n;
+    if(n % 2 == 0 && n > 2) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;                       // <- descomenta si hay multiples casos
+    for (int tc = 1; tc <= T; ++tc) {
+        // cout << "Case #" << tc << ": ";
+        solve();
+    }
+    return 0;
+}
